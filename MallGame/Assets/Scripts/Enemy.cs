cba9976128c5fd4c329527c5d6e9ef235d3e2cac
gearup117 <yashwankhade987@gmail.com>
2020-08-08 17:10:10 +0530

    idk

--- MallGame/Assets/Scripts/Enemy.cs
+++ MallGame/Assets/Scripts/Enemy.cs
@@ -5,6 +5,9 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    public Color redColor,blueColor;
+    private Color currentColor;
+    public Light light;
     private NavMeshAgent nav;
     private Transform player;
     // Start is called before the first frame update
@@ -14,18 +17,31 @@ public class Enemy : MonoBehaviour
         nav = GetComponent<NavMeshAgent>();
          nav.updateUpAxis = false;
        // nav.updateRotation = false;
+
        
         
     }
     void Start()
     {
-       
+        currentColor = redColor;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (currentColor == redColor)
+        {
+            currentColor = blueColor;
+            
+        }
+        else {
+            currentColor = redColor;
+            
+        }
+        light.color = currentColor;
         nav.SetDestination(player.position);
+       // light.color = Color.Lerp(light.color, currentColor, 1);
+        
     }
 }
