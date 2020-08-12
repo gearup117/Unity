5c5fea78fc48d0e337521ce62b153ebee604c647
gearup117 <yashwankhade987@gmail.com>
2020-08-12 22:49:55 +0530

    idk

---
+++ MallGame/Assets/Scripts/PickList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickList : MonoBehaviour
+{
+    public Sprite[] pickAbleList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var image = gameObject.GetComponent<Image>();
+        image.sprite = pickAbleList[Random.Range(0, pickAbleList.Length)];
+        Debug.Log(image.sprite.name);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
