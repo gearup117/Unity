a8635176a013ee945c00bfc25317809873201466
gearup117 <yashwankhade987@gmail.com>
2020-08-25 18:21:29 +0530

    idk

---
+++ BadaSoch/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float health;
+    public NavMeshAgent navMesh;
+    public Animator anim;
+    bool canWalk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canWalk = false;
+        Invoke("walk", Random.Range(3f, 5f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0) {
+            navMesh.speed = 0;
+            anim.SetBool("Death", true);
+            Invoke("death", 2f);
+        }
+        
+        if (canWalk && health > 0) {
+            var player = GameObject.FindGameObjectWithTag("Player").transform;
+            navMesh.SetDestination(player.position);
+        }
+
+    }
+
+    void walk() {
+        canWalk = true;
+        anim.SetBool("Walk", true);
+    }
+    void death()
+    {
+        
+        Destroy(this.gameObject);
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Bullet") {
+            Destroy(collision.gameObject);
+            health -= 1f;
+            
+        }
+    }
+}
--- BadaSoch/Assets/Scripts/PlayerMovement.cs
+++ BadaSoch/Assets/Scripts/PlayerMovement.cs
@@ -70,7 +70,7 @@ public class PlayerMovement : MonoBehaviour
         
         
         
-        Debug.Log(x);
+        
 
         
 
