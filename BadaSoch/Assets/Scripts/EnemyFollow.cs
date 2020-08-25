9cf5b30dbc916c9a1b64994608b988f93aae7870
gearup117 <yashwankhade987@gmail.com>
2020-08-25 22:55:22 +0530

    EnemyPatrol

--- BadaSoch/Assets/Scripts/EnemyFollow.cs
+++ BadaSoch/Assets/Scripts/EnemyFollow.cs
@@ -5,6 +5,11 @@ using UnityEngine.AI;
 
 public class EnemyFollow : MonoBehaviour
 {
+    public int patrolPointIndex;
+    public float viewRadius;
+    bool sawPlayer,reachedDest;
+    public Transform[] patrolPoints;
+    public LayerMask playerMask;
     public GameObject blood,bloodPoint;
     public float health;
     public NavMeshAgent navMesh;
@@ -13,30 +18,71 @@ public class EnemyFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        patrolPointIndex = Random.Range(0, patrolPoints.Length);
+        
         canWalk = false;
         Invoke("walk", Random.Range(3f, 5f));
     }
-
+    void walk()
+    {
+        patrol();
+        canWalk = true;
+        anim.SetBool("Walk", true);
+    }
     // Update is called once per frame
     void Update()
     {
         if (health <= 0) {
+            
             navMesh.speed = 0;
+            
             anim.SetBool("Death", true);
             Invoke("death", 2f);
         }
-        
-        if (canWalk && health > 0) {
-            var player = GameObject.FindGameObjectWithTag("Player").transform;
-            navMesh.SetDestination(player.position);
+
+        if (sawPlayer == false)
+        {
+            sawPlayer = Physics.CheckSphere(transform.position, viewRadius, playerMask);
         }
+        
+        if (canWalk && health > 0)
+        {
+            
+            if (sawPlayer)
+            {
+                sawPlayer = true;
+                var player = GameObject.FindGameObjectWithTag("Player").transform;
+                navMesh.speed = 4f;
+                navMesh.SetDestination(player.position);
+            }
+            else
+            {
+                patrol();
+            }
+        }
+        
 
     }
-
-    void walk() {
-        canWalk = true;
-        anim.SetBool("Walk", true);
+    void setNewPatrolPoint() {
+        patrolPointIndex = Random.Range(0, patrolPoints.Length);
     }
+    void patrol() {
+        if (transform.position.x == patrolPoints[patrolPointIndex].position.x && transform.position.z == patrolPoints[patrolPointIndex].position.z)
+        {
+            Debug.Log("Reached");
+            setNewPatrolPoint();
+
+        }
+        navMesh.SetDestination(patrolPoints[patrolPointIndex].position);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, viewRadius);
+        //Gizmos.DrawWireCube(colliderCheck.position, new Vector3(1.5f, 1f, 1.5f));
+    }
+    
+    
     void death()
     {
         
